16f42169cc0421d595aa8bdf6631a98979f23c2a
Edward Beesley <edwardjbeesley@gmail.com>
2022-07-15 17:16:49 +0100

    added animation to buttons when they are pressed and clicked

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
-// console.log("connected")
-
+// for clicking on the button
 document.querySelectorAll(".key").forEach ((drum) => {
   drum.addEventListener("click", function() {
     const code = this.getAttribute('data-key');
+    const key = document.querySelector(`.key[data-key="${code}"]`);
     const audio = document.querySelector(`audio[data-key="${code}"]`);
+    audio.currentTime = 0;
     audio.play();
-    // setTimeout(function() {this.classList.remove("playing")}, 100 );
+    key.classList.add('playing');
   });
 });
 
+// For pressing corresponding key
 window.addEventListener('keydown', function(event) {
   const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
   if(!audio) return;
+  audio.currentTime = 0;
   audio.play();
+  key.classList.add('playing');
 })
+
+function removeTransition(event) {
+  if (event.propertyName !== 'transform') return;
+  this.classList.remove('playing')
+}
+
+const keys = document.querySelectorAll('.key');
+
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
