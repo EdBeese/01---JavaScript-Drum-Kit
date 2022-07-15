b1d621be32a7da94ba547004b0fa138d36b717c8
Edward Beesley <edwardjbeesley@gmail.com>
2022-07-15 17:26:05 +0100

    moved playSound function out of event listener

--- index.js
+++ index.js
@@ -11,14 +11,16 @@ document.querySelectorAll(".key").forEach ((drum) => {
 });
 
 // For pressing corresponding key
-window.addEventListener('keydown', function(event) {
+
+
+function playSound(event) {
   const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
   if(!audio) return;
   audio.currentTime = 0;
   audio.play();
   key.classList.add('playing');
-})
+}
 
 function removeTransition(event) {
   if (event.propertyName !== 'transform') return;
@@ -28,3 +30,5 @@ function removeTransition(event) {
 const keys = document.querySelectorAll('.key');
 
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener('keydown', playSound);
